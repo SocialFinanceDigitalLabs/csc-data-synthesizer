493aa7b3e622d16236e4e70d31f586bc05bf91e1
Mark Waddoups <mark.waddoups@socialfinance.org.uk>
2021-06-25 07:58:48 +0100

    Continued work on the manual 903 example

--- examples/manual_903_csv.py
+++ examples/manual_903_csv.py
@@ -1,6 +1,6 @@
 import datetime
 from cscsynth import ChildrenGenerator
-from cscsynth.census import create_header
+from cscsynth.census import create_episodes, create_header
 
 start_date = datetime.datetime(2015, 1, 1)
 end_date = datetime.datetime(2020, 1 ,1)
@@ -12,6 +12,8 @@ children = gen.generate(num_children=5000)
 census_start = datetime.datetime(2017, 4, 1)
 census_end = datetime.datetime(2018, 4, 1)
 
-df = create_header(census_start, census_end, children)
+header_df = create_header(census_start, census_end, children)
+episodes_df = create_episodes(census_start, census_end, children)
 
-df.to_csv('header.csv', index=False)
+header_df.to_csv('header.csv', index=False)
+episodes_df.to_csv('episodes.csv', index=False)
