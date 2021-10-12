808764c988a43ec4da9d8fb3cac2378cb7f628b8
Kaj Siebert <kaj@k-si.com>
2021-10-12 18:26:47 +0100

    Add poetry configuration

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='CSC Data Synthesizer',
-      version='0.1',
-      description='Shared module for generating children services data',
-      author='Social Finance',
-      packages=['cscsynth'],
-      zip_safe=False)
