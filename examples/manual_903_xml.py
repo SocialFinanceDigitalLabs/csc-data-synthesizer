5c083e283d6cf81e0d2df3d9d9cf8570e748e927
MichaelHanksSF <michael.hanks@socialfinance.org.uk>
2022-01-27 11:37:16 +0000

    output dir through pathlib

--- examples/manual_903_xml.py
+++ examples/manual_903_xml.py
@@ -3,6 +3,7 @@ import datetime
 from cscsynth import ChildrenGenerator
 from cscsynth.census import snapshot_children_for_period
 from cscsynth.xml import create_xml
+from pathlib import Path
 
 start_date = datetime.datetime(2015, 1, 1)
 end_date = datetime.datetime(2020, 1 ,1)
@@ -15,5 +16,6 @@ census_start = datetime.datetime(2017, 4, 1)
 census_end = datetime.datetime(2018, 4, 1)
 children = snapshot_children_for_period(census_start, census_end, all_children)
 
-output_path = os.path.join(os.path.dirname(__file__), 'output', 'fake_903.xml')
-create_xml(children, output_path)
+output_path = Path(__file__).parent / "../output"
+output_path.mkdir(parents=True, exist_ok=True)
+create_xml(children, output_path/"fake_903.xml")
